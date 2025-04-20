bc0ff1b7288f3b8f4f32bf670362f76faeccd24b
MrJacob12 <jakub.buciuto@gmail.com>
2025-04-20 21:10:56 +0200

    Add manualChunks configuration to optimize build output for node_modules

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,15 @@ export default defineConfig(({ mode }) => ({
           }
           return "assets/[name]-[hash].[ext]";
         },
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
     },
   },
